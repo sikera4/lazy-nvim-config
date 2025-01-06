2a4fea4fbf465a5be4576aa1accea449c5662dde
andrey.sikera <andrey.sikera@onetwotrip.com>
2025-01-06 18:15:57 +0300

    show dotted files and change gitignored files style

--- lua/plugins/file-explorer.lua
+++ lua/plugins/file-explorer.lua
@@ -2,5 +2,11 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = function(_, opts)
     opts.window.position = "right"
+    opts.filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+      }
+    }
   end,
 }
