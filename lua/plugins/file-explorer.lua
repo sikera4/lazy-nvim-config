4fa96324c64f1b155d3243f540d325639f835a2e
andrey.sikera <andrey.sikera@onetwotrip.com>
2025-01-09 10:51:06 +0300

    fix neotree plugin opts customization

--- lua/plugins/file-explorer.lua
+++ lua/plugins/file-explorer.lua
@@ -2,11 +2,9 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = function(_, opts)
     opts.window.position = "right"
-    opts.filesystem = {
-      filtered_items = {
-        visible = true,
-        hide_dotfiles = false,
-      }
+    opts.filesystem.filtered_items = {
+      visible = true,
+      hide_dotfiles = false,
     }
   end,
 }
