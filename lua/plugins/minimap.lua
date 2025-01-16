2c04c8d62689c2fda5222517e67b621282fa72e0
andrey.sikera <andrey.sikera@onetwotrip.com>
2025-01-16 11:02:37 +0300

    configure minimap

--- lua/plugins/minimap.lua
+++ lua/plugins/minimap.lua
@@ -1,3 +1,18 @@
 return {
-  "Isrothy/neominimap.nvim"
+  "Isrothy/neominimap.nvim",
+  version = "v3.*.*",
+  enabled = true,
+  lazy = false,
+  init = function()
+    -- The following options are recommended when layout == "float"
+    vim.opt.wrap = false
+    vim.opt.sidescrolloff = 36 -- Set a large value
+
+    vim.g.neominimap = {
+      auto_enable = true,
+      float = {
+        window_border = "none",
+      }
+    }
+  end,
 }
